b0e64ab281ec3bb6a773fbf8737a25fd603ef2c3
yuxuehong <2808723214@qq.com>
2023-08-23 09:31:30 +0800

    selenium+appium自动化测试实践代码

--- appauto/封装的代码.py
+++ appauto/封装的代码.py
@@ -1,14 +1,20 @@
 # author_='Yuxuehong';
 # date: 2023/8/17 16:38
+from selenium.webdriver.support.wait import WebDriverWait
 
-from selenium.webdriver.support.wait import WebDriverWait as Wait
 from selenium.webdriver.support import expected_conditions as EC
 
-def Wait_visibity(d, b, v):
+
+def wait_clickable(d, b, v):
     l_ = (b, v)
-    return Wait(d, 10).until(EC.visibility_of_element_located(l_))
+    return WebDriverWait(d, 10).until(EC.element_to_be_clickable(l_))
 
 
-def Wait_visibity_all(d, b, v):
+def wait_visibility(d, b, v):
     l_ = (b, v)
-    return Wait(d, 10).until(EC.visibility_of_all_elements_located(l_))
+    return WebDriverWait(d, 10).until(EC.visibility_of_element_located(l_))
+
+
+def wait_visibility_all(d, b, v):
+    l_ = (b, v)
+    return WebDriverWait(d, 10).until(EC.visibility_of_all_elements_located(l_))
